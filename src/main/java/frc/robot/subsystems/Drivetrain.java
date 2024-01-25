d1dcaff0fe5ea8e4b8b88c6f3deb6b6fbad1fb23
mitchell6744 <mitchellrm@msdr9.org>
2024-01-25 13:42:35 -0600

    Remove encoders, repair arcadeDrive command

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,35 +4,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
-import edu.wpi.first.wpilibj.Encoder;
-
+//import edu.wpi.first.wpilibj.Encoder;
 import com.revrobotics.CANSparkMax;
+import java.util.function.DoubleSupplier;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import java.util.function.DoubleSupplier;
 
 
 public class Drivetrain extends SubsystemBase {
 
-  // must create groups and add 2 motors, assign CANID in constants - RM
+  // The drive motors
   private final CANSparkMax leftMotor1 = new CANSparkMax(DrivetrainConstants.kLeftMotorCANID, MotorType.kBrushless);
   private final CANSparkMax leftMotor2 = new CANSparkMax(DrivetrainConstants.kLeftMotor2CANID, MotorType.kBrushless);
   private final CANSparkMax rightMotor1 = new CANSparkMax(DrivetrainConstants.kRightMotorCANID, MotorType.kBrushless);
   private final CANSparkMax rightMotor2 = new CANSparkMax(DrivetrainConstants.kRightMotor2CANID, MotorType.kBrushless);
 
+  // The robot's drive
   private final DifferentialDrive m_drive = new DifferentialDrive(leftMotor1,rightMotor1);
-
-  private final Encoder m_RightEncoder = new Encoder(
-    DrivetrainConstants.kRightEncoderPort[0],
-    DrivetrainConstants.kRightEncoderPort[1],
-    DrivetrainConstants.kRightEncoderrevesed
-  );
-
-  private final Encoder m_LeftEncoder = new Encoder(
-    DrivetrainConstants.kLeftEncoderPort[0],
-    DrivetrainConstants.kLeftEncoderPort[1],
-    DrivetrainConstants.kLeftEncoderReversed
-  );
 
 
   /** Creates a new subsystem. */
@@ -44,32 +32,21 @@ public class Drivetrain extends SubsystemBase {
     
     rightMotor1.setInverted(true);
   }
-
+/* 
   public Command arcadeDrive(double FWD, double Rotate){
-    return runOnce(()->{
+    return run(()->{
       m_drive.arcadeDrive(FWD, Rotate);
     });
   }
-  /* 
-  public Command driveDistanceCommand(double distanceMeters, double speed) {
-    return runOnce(
-            () -> {
-              // Reset encoders at the start of the command
-              m_leftEncoder.reset();
-              m_rightEncoder.reset();
-            })
-        // Drive forward at specified speed
-        .andThen(run(() -> m_drive.arcadeDrive(speed, 0)))
-        // End command when we've traveled the specified distance
-        .until(
-            () ->
-                Math.max(m_leftEncoder.getDistance(), m_rightEncoder.getDistance())
-                    >= distanceMeters)
-        // Stop the drive when the command ends
-        .finallyDo(interrupted -> m_drive.stopMotor());
-  }*/
+*/
+  public Command arcadeDriveCommand(DoubleSupplier fwd, DoubleSupplier rot) {
+    // A split-stick arcade command, with forward/backward controlled by the left
+    // hand, and turning controlled by the right.
+    return run(() -> m_drive.arcadeDrive(fwd.getAsDouble(), rot.getAsDouble()))
+        .withName("arcadeDrive");
+  }
 
-
+/*
   public Command driveDistanceCommand(double DistanceM, double Speed){
     return runOnce(() -> {
       m_RightEncoder.reset();
@@ -78,8 +55,9 @@ public class Drivetrain extends SubsystemBase {
     .andThen(run(() -> m_drive.arcadeDrive(Speed, 0))
     .until(() -> Math.max(m_LeftEncoder.getDistance(), m_RightEncoder.getDistance()) >= DistanceM))
     .finallyDo(interuppted -> m_drive.stopMotor());
+     
   }
-  
+  */
   public Command driveRotateCommand(double degrees, double Speed){
     return runOnce(() ->{
         m_drive.arcadeDrive(Speed, degrees);
