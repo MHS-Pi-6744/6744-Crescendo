6ba6e810548ded1e11b2210f1cfc773d54c7a14a
Leo <149526582+6744Leo@users.noreply.github.com>
2024-01-27 12:39:34 -0600

    commit

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,17 +4,24 @@ package frc.robot.subsystems;
 //import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.SparkmaxArmConstants;
 import frc.robot.Constants.UpperIntakeConstants;
 
 //import edu.wpi.first.wpilibj.DigitalInput;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 //import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ArmSubsystem extends SubsystemBase {
     private final WPI_TalonSRX m_armMotor = new WPI_TalonSRX(UpperIntakeConstants.kArmMotorCANID);
-    //private final DigitalInput topLimitSwitch = new DigitalInput(0);
+    private final CANSparkMax m_sparkMotor = new CANSparkMax(SparkmaxArmConstants.kSparkMaxCANID, MotorType.kBrushless);
+    
+    
 
     static final double armSpeed = .25;
+
+    
     
     
     public Command retractArmCommand(){
