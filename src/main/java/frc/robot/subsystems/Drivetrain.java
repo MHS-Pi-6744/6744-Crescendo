bf6da9ed5afa85923ddbd841084c2cd87bba2598
MattheDev53 <110939089+MattheDev53@users.noreply.github.com>
2024-02-08 14:52:04 -0600

    Not too sure what changed, but something did

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,16 +1,17 @@
 
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+//import edu.wpi.first.wpilibj.Encoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
-//import edu.wpi.first.wpilibj.Encoder;
-import com.revrobotics.CANSparkMax;
-import java.util.function.DoubleSupplier;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Drivetrain extends SubsystemBase {
