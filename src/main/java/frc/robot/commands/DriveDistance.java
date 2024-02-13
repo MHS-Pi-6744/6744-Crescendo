6684e1eb69f9e0e4c072840b99f14460ba3f3950
Coach Mitchell <125908398+mitchell6744@users.noreply.github.com>
2024-02-13 15:35:06 -0600

    Create DriveDistance.java

---
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class DriveDistance extends Command {
+    private final Drivetrain m_drive;
+
+
+    
+}
