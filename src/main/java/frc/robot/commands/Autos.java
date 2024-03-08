bfe446622ff1e1d5784c0d0b2e967537a3e12622
MattheDev53 <matthew.c.albrecht@gmail.com>
2024-03-08 10:44:15 -0600

    Set up Simple Auto, Needs Testing

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,10 +1,18 @@
 package frc.robot.commands;
 
-/*import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Constants.AutoConstants;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.IntakeSubsystem;
 
-public final class Autos{
-*/   
+public class Autos extends SequentialCommandGroup{
+    public Autos(Drivetrain drive){
+        addCommands(
+            new DriveDistance(0.5, 0.3, drive),
+            new DriveRotation(0.5, 0.3, drive),
+            new DriveDistance(0.5, 0.3, drive),
+            new DriveDistance(0.5, -0.3, drive),
+            new DriveRotation(0.5, -0.3, drive),
+            new DriveDistance(0.5, -0.3, drive)
+        );
+    }
+    
+}
