75a3727aac7ba46c74cb1f847dcac3d8ac210aff
MattheDev53 <matthew.c.albrecht@gmail.com>
2024-03-08 11:00:51 -0600

    Changed File name

--- src/main/java/frc/robot/commands/TestAuto.java
+++ src/main/java/frc/robot/commands/TestAuto.java
@@ -3,8 +3,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 
-public class Autos extends SequentialCommandGroup{
-    public Autos(Drivetrain drive){
+public class TestAuto extends SequentialCommandGroup{
+    public TestAuto(Drivetrain drive){
         addCommands(
             new DriveDistance(0.5, 0.3, drive),
             new DriveRotation(0.5, 0.3, drive),
